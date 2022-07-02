dcc08915d6b82d814c9b334e242835150ace2e44
YAYAYru <AlexeyAYAYA@gmail.com>
2022-07-02 21:08:53 +0700

    test2_acc: 0.7042, params: - method: "undersampling_EditedNearestNeighbours_ClusterCentroids" -  test2_loss: 2.4176

--- big/src/stages/make_imbalance.py
+++ big/src/stages/make_imbalance.py
@@ -6,29 +6,118 @@ import numpy as np
 
 from collections import Counter
 # from imblearn.over_sampling import RandomOverSampler
-from imblearn.under_sampling import ClusterCentroids
+from sklearn.linear_model import LogisticRegression
+from imblearn.under_sampling import ClusterCentroids, \
+                                    AllKNN, \
+                                    EditedNearestNeighbours, \
+                                    NearMiss, \
+                                    RandomUnderSampler
 
-def undersampling(df):
-    column_names = df.columns
-    print("column_names", column_names[-5:])    
+
+class Df2np2df:
+    def df2np(self, df):
+        self.column_names = df.columns
+        X = df[df.columns.difference(['fsw', 'videoframe', 'filename', 'foldername'])]
+        y = df["fsw"].to_numpy()
+        return X, y
+        
+    def np2df(self, X_resampled, y_resampled):
+        df_resampled = pd.DataFrame(X_resampled, columns=self.column_names[:-4])
+        df_resampled["fsw"] = y_resampled
+        return df_resampled
+
+
+def undersampling_ClusterCentroids(df):
+    print("-----------------undersampling_ClusterCentroids()------------------------")
+    df2np2df = Df2np2df()
+    X, y = df2np2df.df2np(df)
+
     ros = ClusterCentroids(random_state=0)
-
-    #X = df.loc[:, df.columns != "fsw"]
-    X = df[df.columns.difference(['fsw', 'videoframe', 'filename', 'foldername'])]
-    y = df["fsw"].to_numpy()
-    
-    print("X", X.shape)
-    print("y", y.shape)
     X_resampled, y_resampled = ros.fit_resample(X, y)
-    print("X_resampled", X_resampled.shape)
-    print("y_resampled", y_resampled.shape)    
 
-    df_resampled = pd.DataFrame(X_resampled, columns=column_names[:-4])
+    df_resampled = df2np2df.np2df(X_resampled, y_resampled)
 
-    df_resampled["fsw"] = y_resampled
+    return df_resampled
 
-    print("df_resampled", df_resampled)
-    
+
+def undersampling_AllKNN(df):
+    print("-----------------undersampling_AllKNN()------------------------")
+    df2np2df = Df2np2df()
+    X, y = df2np2df.df2np(df)
+
+    ros = AllKNN()
+    X_resampled, y_resampled = ros.fit_resample(X, y)
+
+    df_resampled = df2np2df.np2df(X_resampled, y_resampled)
+
+    return df_resampled
+
+
+def undersampling_EditedNearestNeighbours(df):
+    print("-----------------undersampling_EditedNearestNeighbours()------------------------")
+    df2np2df = Df2np2df()
+    X, y = df2np2df.df2np(df)
+
+    ros = EditedNearestNeighbours(kind_sel="mode")
+    X_resampled, y_resampled = ros.fit_resample(X, y)
+
+    df_resampled = df2np2df.np2df(X_resampled, y_resampled)
+    return df_resampled
+
+
+def undersampling_EditedNearestNeighbours_ClusterCentroids(df):
+    print("-----------------undersampling_EditedNearestNeighbours_ClusterCentroids()------------------------")
+
+    df2np2df = Df2np2df()
+    X, y = df2np2df.df2np(df)
+
+    enn = EditedNearestNeighbours(kind_sel="all")
+    X_resampled, y_resampled = enn.fit_resample(X, y)
+
+    ros = ClusterCentroids(random_state=0)
+    X_resampled, y_resampled = ros.fit_resample(X_resampled, y_resampled)
+
+    df_resampled = df2np2df.np2df(X_resampled, y_resampled)
+    return df_resampled
+
+
+def undersampling_AllKNN_ClusterCentroids(df):
+    print("-----------------undersampling_AllKNN_ClusterCentroids()------------------------")
+
+    df2np2df = Df2np2df()
+    X, y = df2np2df.df2np(df)
+
+    allknn = AllKNN()
+    X_resampled, y_resampled = allknn.fit_resample(X, y)
+
+    ros = ClusterCentroids(random_state=0)
+    X_resampled, y_resampled = ros.fit_resample(X_resampled, y_resampled)
+
+    df_resampled = df2np2df.np2df(X_resampled, y_resampled)
+    return df_resampled
+
+
+def undersampling_NearMiss(df):
+    print("-----------------undersampling_NearMiss()------------------------")
+    df2np2df = Df2np2df()
+    X, y = df2np2df.df2np(df)
+
+    ros = NearMiss(version=2)
+    X_resampled, y_resampled = ros.fit_resample(X, y)
+
+    df_resampled = df2np2df.np2df(X_resampled, y_resampled)
+    return df_resampled
+
+
+def undersampling_RandomUnderSampler(df):
+    print("-----------------undersampling_RandomUnderSampler()------------------------")
+    df2np2df = Df2np2df()
+    X, y = df2np2df.df2np(df)
+
+    ros = RandomUnderSampler(random_state=0)
+    X_resampled, y_resampled = ros.fit_resample(X, y)
+
+    df_resampled = df2np2df.np2df(X_resampled, y_resampled)
     return df_resampled
 
 @click.command()
@@ -48,8 +137,20 @@ def make_imbalance(path_params_yaml: str):
     
 
     if method=="undersampling_ClusterCentroids":
-        df = undersampling(df)
-    
+        df = undersampling_ClusterCentroids(df)
+    if method=="undersampling_AllKNN":
+        df = undersampling_AllKNN(df)
+    if method=="undersampling_EditedNearestNeighbours":
+        df = undersampling_EditedNearestNeighbours(df)
+    if method=="undersampling_EditedNearestNeighbours_ClusterCentroids":
+        df = undersampling_EditedNearestNeighbours_ClusterCentroids(df)
+    if method=="undersampling_AllKNN_ClusterCentroids":
+        df = undersampling_AllKNN_ClusterCentroids(df)
+    if method=="undersampling_NearMiss":
+        df = undersampling_NearMiss(df)
+    if method=="undersampling_RandomUnderSampler":
+        df = undersampling_RandomUnderSampler(df)
+
     c = Counter(df["fsw"])
     print(f'Distribution balancing classes\n: {c}')   
     df.to_csv(path_train_val_test_csv_balance, index=False)
